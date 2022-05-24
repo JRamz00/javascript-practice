497faef859c3e859c3c3c400f62d98ad20d2ed0f
JRamz00 <95388325+JRamz00@users.noreply.github.com>
2022-05-24 18:39:48 -0400

    created reverse order file

---
+++ reverse_order.js
@@ -0,0 +1,8 @@
+// .length returns the amount of items in the array
+// create new branch and constantly create different branches
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+console.log(numbers)
+
+for(var i = 0; i < numbers.length; i++){
+    console.log(numbers[i])
+}
