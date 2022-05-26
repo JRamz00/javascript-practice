79666ccdc422802efb17368d019e61ac9d244ed2
JRamz00 <95388325+JRamz00@users.noreply.github.com>
2022-05-26 15:47:36 -0400

    new file

---
+++ arbitrary_number_of_integers.js
@@ -0,0 +1,7 @@
+//Design and implement an application that reads an arbitrary number of integers that are in the range 0 t0 50
+//inclusive and counts how many occurrences of each are entered. After all input has been processed, print all of
+//the values (with the number of occurrences) that were entered one or more times.
+//Extra credit: Modify the programs so that it works for numbers in the range between -25 and 25.
+
+const numbers = [0,50]
+console.log(number)
