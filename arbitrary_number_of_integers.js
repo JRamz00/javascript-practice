8f8828ff844a4d5f2df2ffcfeafd61a4562ad12c
JRamz00 <95388325+JRamz00@users.noreply.github.com>
2022-05-31 17:45:00 -0400

    forgot the okld branch

--- arbitrary_number_of_integers.js
+++ arbitrary_number_of_integers.js
@@ -3,5 +3,5 @@
 //the values (with the number of occurrences) that were entered one or more times.
 //Extra credit: Modify the programs so that it works for numbers in the range between -25 and 25.
 
-const numbers = [0,50]
-console.log(number)
+const numbers = [0-50]
+console.log(numbers)
