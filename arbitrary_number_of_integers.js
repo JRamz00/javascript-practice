320daded5f2e34cb49b6bc2f123ccab0eb9da16a
JRamz00 <95388325+JRamz00@users.noreply.github.com>
2022-05-31 17:48:09 -0400

    making a new branch

--- arbitrary_number_of_integers.js
+++ arbitrary_number_of_integers.js
@@ -3,5 +3,22 @@
 //the values (with the number of occurrences) that were entered one or more times.
 //Extra credit: Modify the programs so that it works for numbers in the range between -25 and 25.
 
-const numbers = [0-50]
-console.log(numbers)
+//step 1 make a function that takes in numbers
+//step 2 make a loop that loops through each number and determines if it
+
+
+
+
+function arbNumbers(num) {
+     var counter = 0
+    for (var i = 0; i < arbNumbers.length ; i++) {
+        console.log(arbNumbers.length)
+
+        
+        
+    }
+
+}
+
+
+
