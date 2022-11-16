a2c2ed09e83403a1330e19f7499fe00733cb760d
JRamz00 <95388325+JRamz00@users.noreply.github.com>
2022-11-15 19:44:18 -0500

    omfg

--- blackjack.js
+++ blackjack.js
@@ -8,26 +8,71 @@
 //3. last function is going to deal a card, take the first card/element of the array at the top. Call it deal
 //hint get an object into an array. create 1 object first
 
-makeDeck()
+//1.
+//makeDeck()
 
 function makeDeck(){
     var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     var suits = ["spades", "diamonds", "clubs", "hearts"]
-    var card = {
-        Value: values[" "],
-        Suit: suits[" "]
+    const cardArray = []  
+    for(var i = 0; i < suits.length; i++){
+        for(var j = 0; j < values.length; j++){
+            var card = {
+                Value: values[j],
+                Suit: suits[i]
+            }
+            cardArray.push(card)
+        }     
+    }    
+    return cardArray
+}  
+
+function shuffleDeck(deck){
+    for(var i = 0; i < deck.length; i++){
+        var storage = deck[i]
+        var randomIndex = getRandomInt(deck.length-1)
+        deck[i] = deck[randomIndex]
+        deck[randomIndex] = storage
     }
-    console.log(card)  
-    for(var i = 0; i < suits.length; i++ ){
-        for(var j = 0; j <= values.length; j++){
-            console.log(i, j)
-            console.log(card)
+    return deck
+}
 
+// function getHands(deck){
+//     const myCards = []
+//     const dealerCards = []
+//     for(var i = 0; i < 2; i++){
+//         var myCard = deck.shift()
+//         var dealerCard = deck.shift()
+//         myCards.push(myCard)
+//         dealerCards.push(dealerCard)
+//     }
+//     return [myCards, dealerCards]
+// }
+// 
+function dealCard(deck){
+    var card = deck.shift()
+    var results = {
+        Card: card,
+        Deck: deck
     }
+    return results
 }
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
 
 
-  
+// FUNCTION CALLS!!
+var deckOfCards = makeDeck()
+console.log(deckOfCards)
 
+var shuffledDeck = shuffleDeck(deckOfCards)
+console.log(shuffledDeck)
 
+// var hands = getHands(shuffledDeck)
+// console.log("Hands ", hands)
+
+var dealtCardAndDeck = dealCard(shuffledDeck)
+console.log("dealt card ", dealtCardAndDeck.Card)
+console.log("Deck after the card has been dealt ", dealtCardAndDeck.Deck)
